f2b9cb6082bda065bcad8b0d04a05af14912c1fb
daminaselli <89040786+daminaselli@users.noreply.github.com>
2021-09-16 20:23:00 -0300

    1er Clase

---
+++ app.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Ingresar Nombre");
+let apellido = prompt("Ingresar Apellido");
+let edad = prompt("Ingresar Edad");
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+alert(nombre + " " + apellido + " " + edad);
