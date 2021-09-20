22b67e5b06d613be7b239bcea0b72f0e430f8077
daminaselli <89040786+daminaselli@users.noreply.github.com>
2021-09-19 21:13:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 let nombre = prompt("Ingresar Nombre");
 let apellido = prompt("Ingresar Apellido");
 let edad = prompt("Ingresar Edad");
+let contraseña = "TrazoDulce"
+let pass = prompt("Ingresar contraseña")
 console.log(nombre);
 console.log(apellido);
-console.log(edad);
-alert(nombre + " " + apellido + " " + edad);
+console.log(edad)
+if(edad >= 18 && pass == contraseña){
+    alert("Bienvenido/a");
+} else if (edad <= 18 && pass != contraseña){
+    alert("No se han podido validar los datos")
+} else if (edad < 18){
+    alert("Sos menor de edad, no puede ingresar")
+} else if (pass != contraseña){
+    alert("Error al ingresar la contraseña")
+} 
