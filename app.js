4dd6f47c85e80b0e2b69b56c0eee1cf27c4b3613
daminaselli <89040786+daminaselli@users.noreply.github.com>
2021-10-03 11:58:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ while (pass != contraseña && intentos > 0){
         }
     }
 }
-*/
 const suma = (a,b) => a + b;
 const resta = (a,b) => a - b;
 const iva = x => x * 0.21;
@@ -29,3 +28,25 @@ let precioProducto = Number(prompt (`Ingresar Precio`));
 let precioDescuento = Number(prompt (`Ingresar Descuento`));  
 let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), precioDescuento); 
 alert (nuevoPrecio);
+*/
+
+class Productos {
+    constructor(nombreP, precioP, stockP) {
+        this.nombre  = nombreP;
+        this.precio  = precioP;
+        this.stock = stockP;
+        this.disponible = true;
+    }
+    comprar(cantidad) {
+        if (this.stock <= 0) {
+            console.log("Stock Agotado")
+            this.disponible = false
+        } else {
+            this.stock = this.stock - cantidad
+            console.log (`Compraste ${this.nombre} a ${this.precio * cantidad}`)
+        }
+    }
+}
+const producto1 = new Productos("Tarta Frutilla", "$1200" , 20 , true);
+const producto2 = new Productos("Cheesecake", "1500", 20, true);
+
