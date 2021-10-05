45767b757904ce929b141bf457e4e8c3cffb3c5a
daminaselli <89040786+daminaselli@users.noreply.github.com>
2021-10-05 20:27:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 class Productos {
-    constructor(nombreP, precioP, stockP, disponibleP) {
-        this.nombre  = nombreP;
-        this.precio  = precioP;
-        this.stock = stockP;
-        this.disponible = disponibleP;
+    constructor(nombre, precio, stock, disponible) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.disponible = disponible;
     }
     comprar(cantidad) {
         if (this.stock <= 0) {
@@ -15,24 +15,37 @@ class Productos {
         }
     }
 }
-const producto1 = new Productos("Tarta Frutilla", "$1200" , 20 , true);
-const producto2 = new Productos("Cheesecake", "1500", 20, true);
+const producto1 = new Productos("Tarta Frutilla", 1200, 20, true)
+const producto2 = new Productos("Lemon Pie", 1200, 20, true)
+console.log(producto1.comprar)
+console.log(producto2.comprar)
 
 /*
-let listaProductos[]
+let listaProductos = [];
 
-listaProductos.push(new Productos("Tarta Frutilla", "$1200" , 20 , true))
-listaProductos.push(new Productos("Cheesecake Frutilla", "$1500" , 20 , true))
-listaProductos.push(new Productos("Dripcake", "$2000" , 20 , true))
-listaProductos.push(new Productos("Number Cake", "$1800" , 20 , true))
-listaProductos.push(new Productos("Lemon Pie", "$1200" , 20 , true))
-listaProductos.push(new Productos("Tarta Oreo", "$1300" , 20 , true))
-listaProductos.push(new Productos("Tarta Frutilla", "$1200" , 20 , true))
+listaProductos.push(new Productos("Tarta Frutilla", 1200 , 20 , true))
+listaProductos.push(new Productos("Cheesecake Frutilla", 1500 , 20 , true))
+listaProductos.push(new Productos("Dripcake", 2000 , 20 , true))
+listaProductos.push(new Productos("Number Cake", 1800 , 20 , true))
+listaProductos.push(new Productos("Lemon Pie", 1200 , 20 , true))
+listaProductos.push(new Productos("Tarta Oreo", 1300 , 20 , true))
+listaProductos.push(new Productos("Tarta Frutilla", 1200 , 20 , true))
+
+listaProductos.sort((a,b) => {
+    if(a.nombre > b.nombre) {
+        return 1
+    }
+    if (a.nombre < b.nombre){
+        return -1
+    }
+    return 0
+})
 
 let search = prompt("¿Qué querés comprar");
 let buscadorMultiple = listaProductos.filter(obj => obj.nombre === search)
 alert(buscadorMultiple);
 */
+
 
 
 
