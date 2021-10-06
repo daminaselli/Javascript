0b0740e8833753df502d60fe214fb863829fc3c5
daminaselli <89040786+daminaselli@users.noreply.github.com>
2021-10-05 21:48:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ class Productos {
     }
     comprar(cantidad) {
         if (this.stock <= 0) {
-            console.log("Stock Agotado")
+            console.log ("Stock Agotado")
             this.disponible = false
         } else {
             this.stock = this.stock - cantidad
@@ -15,13 +15,12 @@ class Productos {
         }
     }
 }
-const producto1 = new Productos("Tarta Frutilla", 1200, 20, true)
-const producto2 = new Productos("Lemon Pie", 1200, 20, true)
-console.log(producto1.comprar)
-console.log(producto2.comprar)
+const producto1 = new Productos("Tarta Frutilla", 1200, 20, true);
+const producto2 = new Productos("Lemon Pie", 1200, 20, true);
+console.log(producto1.comprar);
+console.log(producto2.comprar);
 
-/*
-let listaProductos = [];
+const listaProductos = [];
 
 listaProductos.push(new Productos("Tarta Frutilla", 1200 , 20 , true))
 listaProductos.push(new Productos("Cheesecake Frutilla", 1500 , 20 , true))
@@ -29,7 +28,7 @@ listaProductos.push(new Productos("Dripcake", 2000 , 20 , true))
 listaProductos.push(new Productos("Number Cake", 1800 , 20 , true))
 listaProductos.push(new Productos("Lemon Pie", 1200 , 20 , true))
 listaProductos.push(new Productos("Tarta Oreo", 1300 , 20 , true))
-listaProductos.push(new Productos("Tarta Frutilla", 1200 , 20 , true))
+listaProductos.push(new Productos("Selva Negra", 1200 , 20 , true))
 
 listaProductos.sort((a,b) => {
     if(a.nombre > b.nombre) {
@@ -41,10 +40,9 @@ listaProductos.sort((a,b) => {
     return 0
 })
 
-let search = prompt("¿Qué querés comprar");
-let buscadorMultiple = listaProductos.filter(obj => obj.nombre === search)
-alert(buscadorMultiple);
-*/
+const search = prompt("¿Qué querés comprar");
+const buscadorMultiple = listaProductos.filter(obj => obj.nombre === search)
+console.log(buscadorMultiple);
 
 
 
