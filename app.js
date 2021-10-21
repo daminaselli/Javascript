082672bebac7c2d2826c351117399b332b4c3728
daminaselli <89040786+daminaselli@users.noreply.github.com>
2021-10-21 17:06:10 -0300

    Correcciones

--- app.js
+++ app.js
@@ -1,101 +1,67 @@
-/*class Productos {
-    constructor(nombre, precio, stock, disponible) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.stock = stock;
-        this.disponible = disponible;
-    }
-    comprar(cantidad) {
-        if (this.stock <= 0) {
-            console.log ("Stock Agotado")
-            this.disponible = false
-        } else {
-            this.stock = this.stock - cantidad
-            console.log (`Compraste ${this.nombre} a ${this.precio * cantidad}`)
-        }
+class Productos {
+    constructor ({
+        nombre,
+        email,
+        telefono,
+        producto,
+    }){
+    this.nombre = nombre;
+    this.email = email;
+    this.telefono = telefono;
+    this. producto = producto;
     }
 }
-const producto1 = new Productos("Tarta Frutilla", 1200, 20, true);
-const producto2 = new Productos("Cheesecake Frutilla", 1500 , 20 , true);
-const producto3 = new Productos("Dripcake", 2000 , 20 , true);
-const producto4 = new Productos("Number Cake", 1800 , 20 , true);
-const producto5 = new Productos("Lemon Pie", 1200, 20, true);
-const producto6 = new Productos("Tarta Oreo", 1300 , 20 , true);
-const producto7 = new Productos("Selva Negra", 1200 , 20 , true);
+let listaProductos = [];
 
-console.log(producto1.comprar);
-console.log(producto2.comprar);
-console.log(producto3.comprar);
-console.log(producto4.comprar);
-console.log(producto5.comprar);
-console.log(producto6.comprar);
-console.log(producto7.comprar);
+const agregarProductos = () => {
+    const producto = new Productos ({
+        nombre : document.getElementById ("nombre").value,
+        email : document.getElementById ("email").value,
+        telefono : document.getElementById ("telefono").value,
+        producto : document.getElementById ("producto").value,
+    })
+    return producto
+}
 
-const listaProductos = [];
+const agregarListaProductos = (lista) => {
+    lista.push(agregarProductos())
+    return lista
+}
 
-listaProductos.push(new Productos("Tarta Frutilla", 1200 , 20 , true))
-listaProductos.push(new Productos("Cheesecake Frutilla", 1500 , 20 , true))
-listaProductos.push(new Productos("Dripcake", 2000 , 20 , true))
-listaProductos.push(new Productos("Number Cake", 1800 , 20 , true))
-listaProductos.push(new Productos("Lemon Pie", 1200 , 20 , true))
-listaProductos.push(new Productos("Tarta Oreo", 1300 , 20 , true))
-listaProductos.push(new Productos("Selva Negra", 1200 , 20 , true))
+const guardarBaseDatos = () => {
+    listaProductos = verificarLista()
+    localStorage.setItem("listaProductos", JSON.stringify(listaProductos))
+}
 
-listaProductos.sort((a,b) => {
-    if(a.nombre > b.nombre) {
-        return 1
+const verificarLista = () => {
+    let dato;
+    if(localStorage.setItem("listaProductos") != null) {
+        dato = agregarListaProdcutos(JSON.parse(localStorage.setItem("listaProductos")))
+        return dato
+    } else{
+        agregarListaProductos(listaProductos)
+        dato = listaProductos
+        return dato
     }
-    if (a.nombre < b.nombre){
-        return -1
-    }
-    return 0
+}
+
+const verDatos = () => {
+    verificarLista().forEach(obj => {
+        document.getElementById("tabla").innerHTML += `
+        <tr>
+            <td>${obj.nombre}</td>
+            <td>${obj.email}</td>
+            <td>${obj.telefono}</td>
+            <td>${obj.producto}</td>
+        </tr>
+        `
+    });
+}
+
+document.getElementById("btn").addEventListener("click", (e) => {
+    e.preventDefault
+    guardarBaseDatos()
 })
-
-const search = prompt("¿Qué querés comprar");
-const buscadorMultiple = listaProductos.filter(obj => obj.nombre === search)
-console.log(buscadorMultiple);*/
-
-const h3 = document.createElement ("h3")
-h3.setAttribute("id","titulo")
-h3.textContent = "¿Cuál producto querés?. Rellena el formulario"
-document.getElementById("formulario").appendChild(h3)
-
-class productos {
-    constructor ({nombre,email,producto}) {
-        this.nombre = nombre;
-        this.email = email;
-        this.producto = producto;
-    }
+if(localStorage.getItem("listaProductos") !=null ) {
+    verDatos()
 }
-
-const guardarDatos = () => {
-
-    const product = new productos(
-        {
-        nombre : document.getElementById("nombre").value,
-        email : document.getElementById("email").value,
-        producto : document.getElementById("producto").value
-    }
-    )
-    return product
-}
-document.getElementById("btn").addEventListener("click", () => {
-    guardarDatos()
-})
-const boton = document.getElementById("btn")
-boton.addEventListener("click", (e) => {
-    e.preventDefault()
-    validadoda()
-    if(validadora == true) {
-        document.getElementById("form").submit()
-    }
-    console.log(nombre.value)
-})
-const validadora = () => {
-    if(imput.value.length > 5) {
-        return input.value
-    }
-}
-
-
-
